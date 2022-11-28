a137cd083ac85f2e3e09318d33eb12537c005e1e
TsukiShinen <49118528+TsukiShinen@users.noreply.github.com>
2022-11-28 18:01:41 +0100

    Behaviour tree

---
+++ Assets/Scripts/GameManagerSO.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using DefaultNamespace;
+using Library.Command;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+[CreateAssetMenu(fileName = "GameManagerSO", menuName = "Game/GameManagerSO", order = 0)]
+public class GameManagerSO : ScriptableObject
+{
+    public GameParametersSO gameParametersSO;
+    public Dictionary<TeamSO, int> Scores;
+    public float timer;
+
+
+    public bool TimerFished => timer <= 0;
+
+    public void Init()
+    {
+        timer = gameParametersSO.gameTimer;
+    }
+}
---
+++ Assets/Scripts/GameParametersSO.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [CreateAssetMenu(fileName = "GameParametersSO", menuName = "Game/GameParametersSO", order = 0)]
+    public class GameParametersSO : ScriptableObject
+    {
+        public int maxScore = 100;
+        public float gameTimer = 120f;
+    }
+}
