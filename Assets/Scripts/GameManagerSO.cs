74c2cdb4ad34ea009f70ffda3524b16a4b33abeb
AokoMitsUwU <80577298+AokoMitsuu@users.noreply.github.com>
2022-11-29 19:14:30 +0100

    finish start

--- Assets/Scripts/GameManagerSO.cs
+++ Assets/Scripts/GameManagerSO.cs
@@ -35,13 +35,13 @@ public class GameManagerSO : ScriptableObject
         for (int i = 0; i < team1.TankList.Count; i++)
         {
             team1.TankList[i].PathFinding.grid = grid;
-            var tankObject = Instantiate(tankPrefab, spawnTeam1.transform.position, spawnTeam1.transform.rotation);
+            var tankObject = Instantiate(tankPrefab, spawnTeam1.transform.position + spawnTeam1.transform.right * 3 * i, spawnTeam1.transform.rotation);
             tankObject.GetComponent<Tank>().InitialLoad(team1.TankList[i]);
         }
         for (int i = 0; i < team2.TankList.Count; i++)
         {
             team2.TankList[i].PathFinding.grid = grid;
-            var tankObject = Instantiate(tankPrefab, spawnTeam1.transform.position, spawnTeam1.transform.rotation);
+            var tankObject = Instantiate(tankPrefab, spawnTeam2.transform.position + spawnTeam2.transform.right * 3 * i, spawnTeam2.transform.rotation);
             tankObject.GetComponent<Tank>().InitialLoad(team2.TankList[i]);
         }
     }
