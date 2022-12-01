2e6da007ce8f530c32c9ced82bd45cef495f7013
AokoMitsUwU <80577298+AokoMitsuu@users.noreply.github.com>
2022-12-01 14:42:27 +0100

    tout marche comme sur des chaines

--- Assets/SetIaOrPlayer.cs
+++ Assets/SetIaOrPlayer.cs
@@ -5,6 +5,10 @@ public class SetIaOrPlayer : MonoBehaviour
 {
     [SerializeField] Dropdown dropdown;
     [SerializeField] GameManagerSO gamemanager;
+    private void Start()
+    {
+        gamemanager.isPlayer = dropdown.value == 1;
+    }
     public void SetIaPlayer() {
         gamemanager.isPlayer = dropdown.value == 1;
     }
